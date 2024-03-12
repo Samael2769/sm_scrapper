5e817267bae71ec06d9464dc97b29a02e8be7d8a
Samael <jeremie.joannard@epitech.eu>
2024-03-13 06:41:06 +0900

    [MOD] sources/sm_scrapper.py,
    Can access library

--- sources/sm_scrapper.py
+++ sources/sm_scrapper.py
@@ -10,9 +10,13 @@ import time
 
 def sm_scrapper():
     app = Application(backend="uia").start(r"D:\Games\Steam\steam.exe")
+    time.sleep(2)
     app2 = Application(backend="uia").connect(title="Steam")
     app2.Steam.wait('visible')
-    time.sleep(5)
-    print(app2.windows())
-    #main_dlg = app.window(title="Steam")
-    #main_dlg.wait('visible')
+    time.sleep(2)
+    main_dlg = app2.window(title="Steam")
+    main_dlg.set_focus()
+    #print(main_dlg.print_control_identifiers())
+    library_dlg = main_dlg.child_window(title="LIBRARY")
+    library_dlg.set_focus()
+    library_dlg.click_input()
