31e8fca8d9a7ef36ec76e74ac3db5cb89ff01dc5
Samael <jeremie.joannard@epitech.eu>
2024-03-15 06:07:36 +0900

    [MOD] sources/sm_scrapper.py,
    Interactive system

--- sources/sm_scrapper.py
+++ sources/sm_scrapper.py
@@ -8,30 +8,105 @@
 from pywinauto.application import Application
 import time
 import sys
+import glob
+import os
+
+actual_game = ""
+actual_screen = None
+main_screen = None
+app = None
+
+def start_game(game):
+    global actual_game
+    global main_screen
+    global actual_screen
+    global app
+    actual_game = game[1]
+    try:
+        app = Application(backend="uia").start(actual_game)
+    except:
+        print("Error: game not found")
+        return
+    print("Waiting for game to start")
+    time.sleep(2)
+    try:
+        main_screen = app.window(title=game[2])
+        main_screen.wait('visible')
+    except:
+        try:
+            main_screen = Application(backend="uia").connect(title=game[2])
+            app = main_screen
+            main_screen = main_screen.window(title=game[2])
+            main_screen.wait('visible')
+        except:
+            print("Error: screen not found")
+            return
+    print("Main screen found")
+
+def stop_game(game):
+    app.kill()
+    actual_game = ""
+    actual_screen = None
+    main_screen = None
+
+def find_window(name):
+    global actual_screen
+    try:
+        actual_screen = main_screen.window(title=name[1])
+    except:
+        print("Error: window not found")
+        return
+
+
+def print_game(game):
+    print("Game: " + actual_game)
+
+def print_screen(screen):
+    print(actual_screen.print_control_identifiers())
+
+def print_main_screen(screen):
+    print(main_screen.print_control_identifiers())
+
+def click(screen):
+    try:
+        actual_screen.set_focus()
+        actual_screen.click_input()
+    except:
+        print("Error: button not found")
+        return
+
+def type_keys(screen):
+    try:
+        actual_screen.type_keys(screen[1])
+    except:
+        print("Error: text box not found")
+        return
+
+def help():
+    print("start: start_game")
+    print("stop: stop_game")
+    print("connect: find_window")
+    print("prg: print_game")
+    print("prs: print_screen")
+
+cmd_tab = {
+    "start": start_game,
+    "stop": stop_game,
+    "connect": find_window,
+    "prg": print_game,
+    "prs": print_screen,
+    "prm": print_main_screen,
+    "help": help,
+    "click": click,
+    "type": type_keys
+}
 
 def sm_scrapper():
-    app = Application(backend="uia").start(r"D:\Games\Steam\steam.exe")
-    time.sleep(2)
-    app2 = Application(backend="uia").connect(title="Steam")
-    app2.Steam.wait('visible')
-    time.sleep(2)
-    main_dlg = app2.window(title="Steam")
-    main_dlg.set_focus()
-    #print(main_dlg.print_control_identifiers())
-    library_dlg = main_dlg.child_window(title="LIBRARY")
-    library_dlg.set_focus()
-    library_dlg.click_input()
-    game = sys.argv[1]
-    search_dlg = main_dlg.child_window(title="Search by Name")
-    search_dlg.set_focus()
-    search_dlg.type_keys(game)
-    time.sleep(2)
-    game_dlg = main_dlg.child_window(title=game, control_type="Text", found_index=0)
-    game_dlg.set_focus()
-    game_dlg.click_input()
-    time.sleep(2)
-    #print (main_dlg.print_control_identifiers())
-    play_dlg = main_dlg.child_window(title="PLAY", found_index=3)
-
-    play_dlg.set_focus()
-    play_dlg.click_input()
+    while True:
+        print("prompt $> ", end="")
+        inp = input()
+        parsed_inp = inp.split(" ")
+        if parsed_inp[0] == "exit":
+            break
+        if parsed_inp[0] in cmd_tab:
+            cmd_tab[parsed_inp[0]](parsed_inp)
